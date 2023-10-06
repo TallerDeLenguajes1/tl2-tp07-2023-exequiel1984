4053a481d14718c58e3d3e5e3690a43b1928483e
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2023-10-06 13:32:26 -0300

    Comenzando con la clase tarea

---
+++ Models/Tarea.cs
@@ -0,0 +1,16 @@
+namespace Practico7
+{
+    enum Estados
+    {
+        Pendiente,
+        EnProgreso,
+        Completada
+    }
+    public class Tarea
+    {
+        private int id;
+        private string titulo;
+        private string descripcion;
+
+    }
+}
